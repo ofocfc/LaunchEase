fd8594997825cbac1ba866d475954be8ca21111f
随便 <oncwnuD0ja3_E_4ssEwPqkfCnAd0@git.weixin.qq.com>
2026-08-14 20:32:18 +0800

    Initial Commit

---
+++ MacLaunch/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  MacLaunch
+//
+//  Created by 随便 on 2026/8/14.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ MacLaunch/MacLaunchApp.swift
@@ -0,0 +1,17 @@
+//
+//  MacLaunchApp.swift
+//  MacLaunch
+//
+//  Created by 随便 on 2026/8/14.
+//
+
+import SwiftUI
+
+@main
+struct MacLaunchApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
